c0c62f3f04594f105b492ef9502c174b50820c60
@laerciodev <jose.laercio.bezerra@gmail.com>
2020-01-26 09:32:15 -0200

    Create component slider

---
+++ src/components/Slider/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styles.css';
+
+const Slider = () => (
+    <div className="container-slider">
+        <div className="slides">
+            <div id="slide-1" className="slide">1</div>
+            <div id="slide-2" className="slide">2</div>
+            <div id="slide-3" className="slide">3</div>
+            <div id="slide-4" className="slide">4</div>
+            <div id="slide-5" className="slide">5</div>
+        </div>
+        <div className="container-controls">
+            <a className="control-slide" href="#slide-1"></a>
+            <a className="control-slide" href="#slide-2"></a>
+            <a className="control-slide" href="#slide-3"></a>
+            <a className="control-slide" href="#slide-4"></a>
+            <a className="control-slide" href="#slide-5"></a>
+        </div>
+    </div>
+);
+
+export default Slider;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Menu from '../components/Menu';
+import Slider from '../components/Slider';
 
 const IndexPage = () => (
-    <Menu />
+    <Slider />
 );
 
 export default IndexPage
