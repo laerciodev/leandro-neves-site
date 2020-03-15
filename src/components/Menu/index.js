ce78cd207e4f53a6863369f89a9ae2ffa886abbe
@laerciodev <jose.laercio.bezerra@gmail.com>
2020-03-15 07:58:34 -0300

    create open/close menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,13 +2,22 @@ import React from "react"
 import MenuContainer from './MenuContainer';
 import "./styles.css"
 
-const Menu = () => (
-    <>
-        <nav className="nav-menu">
-            menu
-        </nav>
-        <MenuContainer />
-    </>
-);
+const Menu = () => {
+    
+    const toggleMenu = (e) => {
+        const slider = document.querySelector('.container-slider');
+        slider.classList.toggle('open-menu');
+        e.target.nextSibling.classList.toggle('open-menu');
+    }
+
+    return (
+        <>
+            <nav onClick={toggleMenu} className="nav-menu">
+                menu
+            </nav>
+            <MenuContainer />
+        </>
+    )
+};
 
 export default Menu;
