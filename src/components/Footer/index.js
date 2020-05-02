206882b59b026fdd87f726d3d41f4227c71a8592
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2020-05-01 23:16:04 -0300

    fix variable window

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import MenuContainer from '../Menu/MenuContainer';
-import './styles.css';
+import React from "react"
+import { Link } from "gatsby"
+import MenuContainer from "../Menu/MenuContainer"
+import "./styles.css"
 
 const Footer = () => {
-    const url = window.location.pathname;
+  const url = typeof window !== undefined ? window.location.pathname : ""
 
-    return (
-        <>  
-            { url !== '/' ? <Link to="/" className="btn-back">&lt; Voltar</Link> : null }
-            <footer className="footer">
-                <MenuContainer />
-                <h3 className="social-media-title">Redes sociais</h3>
-                <Link to="/contato" className="email-contact">contato@studioleandroneves.com</Link>
-                <p className="copyright">leandroneves&copy;</p>
-            </footer>
-        </>
-    )
+  return (
+    <>
+      {url !== "/" ? (
+        <Link to="/" className="btn-back">
+          &lt; Voltar
+        </Link>
+      ) : null}
+      <footer className="footer">
+        <MenuContainer />
+        <h3 className="social-media-title">Redes sociais</h3>
+        <Link to="/contato" className="email-contact">
+          contato@studioleandroneves.com
+        </Link>
+        <p className="copyright">leandroneves&copy;</p>
+      </footer>
+    </>
+  )
 }
 
-
-export default Footer;
+export default Footer
