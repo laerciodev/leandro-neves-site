29280be1fc8e3b8eb7729fe5dd7e088f35c76085
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2020-05-02 11:38:50 -0300

    minor fixes

--- src/components/ContentNos/index.js
+++ src/components/ContentNos/index.js
@@ -1,42 +1,44 @@
-import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
-import './styles.css';
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import "./styles.css"
 
 const ContentNos = () => {
-    const { allFile } = useStaticQuery(
-        graphql`
-            query {
-                allFile(filter: {
-                    extension: { regex: "/(jpg)/" }
-                    sourceInstanceName: { eq: "content-nos" }
-                }) 
-                {
-                    edges {
-                        node {
-                            id
-                            childImageSharp {
-                            fluid(maxWidth: 768) {
-                                ...GatsbyImageSharpFluid
-                            }
-                        } 
-                    }
+  const { allFile } = useStaticQuery(
+    graphql`
+      query {
+        allFile(
+          filter: {
+            extension: { regex: "/(jpg)/" }
+            sourceInstanceName: { eq: "content-nos" }
+          }
+        ) {
+          edges {
+            node {
+              id
+              childImageSharp {
+                fluid(maxWidth: 768, quality: 100) {
+                  ...GatsbyImageSharpFluid_withWebp
                 }
+              }
             }
-        }`
-    );
+          }
+        }
+      }
+    `
+  )
 
-    const { node: imgNos } = allFile.edges[0];
-    
-    return (
-        <>
-            <Img fluid={ imgNos.childImageSharp.fluid } key={ imgNos.id } />
-            <p className="text-nos">
-                agora estamos prontos para te escutar... nos conte e daremos
-                início ao seu projeto.
-            </p>
-        </>
-    )
+  const { node: imgNos } = allFile.edges[0]
+
+  return (
+    <>
+      <Img fluid={imgNos.childImageSharp.fluid} key={imgNos.id} />
+      <p className="text-nos">
+        agora estamos prontos para te escutar... nos conte e daremos início ao
+        seu projeto.
+      </p>
+    </>
+  )
 }
 
-export default ContentNos;
+export default ContentNos
